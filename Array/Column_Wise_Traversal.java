382a593aa8b06799abfc268fd64f4faf22d1f228
Hek The Trouble <heksalera1@gmail.com>
2026-01-15 13:42:55 +0800

    Add files via upload

---
+++ Array/Column_Wise_Traversal.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Column_Wise_Traversal {
+
+	public static void main(String[] args) {
+		//  
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter row: ");
+		int row = scan.nextInt();
+		System.out.print("Enter column: ");
+		int column = scan.nextInt();
+		
+		int[][] arr = new int[row][column];
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print("Enter element [" + i + "][" + j +"]: ");
+				arr[i][j] = scan.nextInt();
+			}
+		}
+		
+		System.out.println("Column-wise Traversal: ");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print(arr[j][i]++ + " ");
+			}
+			System.out.println();
+		}
+		scan.close();
+
+	}
+
+}
